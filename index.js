49e7bf26bc93792d778bfc9e7c5f3c4596094cce
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2023-10-29 19:47:43 -0400

    add

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
 const express = require('express')
 const app = express()
-
-// GET /products/all HTTP/1.1, Page: 2, Sort: price, Order: desc
-
-app.get('/products/:productName', (request, response) => {
-    response.send('Product Page! Product name: Sort: price, Order: desc' + request.params.productName)
-})
+const bodyParser = bodyParser.urlencoded({ extended: false})
 
 
-// GET /products/8719 HTTP/1.1.
 
-app.get('/products/:productId', (request, response) => {
-    response.send(
-        'Product id: ' + request.params.productId,
-        )
-})
 
-// GET /products/8719-small-red HTTP/1.1
 
-app.get('/products/:productId-:productName', (request, response) => {
-    response.send(
-        'Product Page! Product name: ' + request.params.productName +
-        'Product id: ' + request.params.productId,
-        )
-})
 
 
 
