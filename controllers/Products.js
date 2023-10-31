941531f01ff049f4111d81ea82ed8330152482a5
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2023-10-31 11:29:19 -0400

    f

--- controllers/Products.js
+++ controllers/Products.js
@@ -2,4 +2,29 @@ const index = (req, res) => {
     res.send('Products.index')
 }
 
-module.exports = { index }
+const form = (req, res) => {
+    res.send('Product.form')
+}
+
+const show = (req, res) => {
+    res.send('Product.show')
+}
+
+const create = (req, res) => {
+    res.send('Product.create')
+}
+
+const update = (req, res) => {
+    res.send('Product.update')
+}
+
+const remove = (req, res) => {
+    res.send('Product.remove')
+}
+
+
+
+
+
+
+module.exports = { index, form, show, create, update, remove }
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-app.use(bodyParser = urlencoded({ extended: false}))
+app.use(bodyParser.urlencoded({ extended: false}))
 const productRouter = require('./routes/Products')
 
 app.use("/products", productRouter)
