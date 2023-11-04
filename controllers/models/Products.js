006e7d2263e9f9fff89f0351924f7e849d62b6fd
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2023-11-04 09:15:53 -0400

    aa

---
+++ controllers/models/Products.js
@@ -0,0 +1,31 @@
+let products = [
+    {id: "1", "slugs": "nike-shoe", "name": "Nike Shoe" }
+]
+
+const all = () => {
+    return products
+}
+
+const find = (id) => {
+    return products.find(p => p.id === id)
+}
+
+const create = (product) => {
+        products.push(product)
+        return products
+    }
+
+
+const update = (id, product) => {
+    products = products.map((p) => {
+        return (p.id === id) ? product : p
+    })
+    return products
+}
+
+const remove = (id) => {
+    products = products.filter(p => p.id !== id)
+    return products
+}
+
+module.exports = { all, find, update, create, remove }
