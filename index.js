5329d74863fc168f4b53c3058c5130fff559f40d
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2023-11-04 10:51:34 -0400

    f

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false}))
 const productRouter = require('./routes/Products')
-app.set('views', __dirname + '/templates/views')
+app.set('views', __dirname + '/templates')
 app.set('view engine', 'twig')
 
 app.get('/', (req, res) => {
