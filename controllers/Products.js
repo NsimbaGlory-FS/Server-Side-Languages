7e3e7538338999c7c630004219b273b90566077d
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2023-11-11 11:30:54 -0500

    add

--- controllers/Products.js
+++ controllers/Products.js
@@ -2,31 +2,42 @@ const Products = require('./models/Products')
 
 const index = (req, res) => {
     const products = Products.all()
-    res.json(products)
+    res.render('views/products/index' , { products })
+    // res.json(products)
 }
 
 const form = (req, res) => {
-    res.send('Product.form')
+    // res.send('Products.form')
+    if (req.params.id) {
+        const product = Products.find(req.params.id)
+        res.render('views/products/edit', { product })
+
+    } else {
+        res.render('views/products/create')
+    }
+
 }
 
 const show = (req, res) => {
     const product = Products.find(req.params.id)
-    res.json(product)
+    //res.json(product)
+    res.render('views/products/show', { product })
 }
 
 const create = (req, res) => {
     const product = Products.create(req.body)
-    res.json(product)
+    res.redirect('/products/' + product.id)
 }
 
 const update = (req, res) => {
     const product = Products.update(req.params.id, req.body)
-    res.json(product)
+    res.redirect('/products/' + req.params.id)
 }
 
 const remove = (req, res) => {
     const products = Products.remove(req.params.id)
-    res.json(products)
+    res.redirect('/products')
+    // res.json(products)
 }
 
 
