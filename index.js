d462ab74319608e02fc8bf5e48e3575eb3801fe2
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2023-11-11 11:26:41 -0500

    add

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const app = express()
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false}))
 const productRouter = require('./routes/Products')
+const variantsRouter = require('./routes/Variants')
+const imagesRouter = require('./routes/Images')
 app.set('views', __dirname + '/templates')
 app.set('view engine', 'twig')
 
 app.get('/', (req, res) => {
-    res.render("home", { name: 'World!', 'users': [
-        { name: 'Justin Worman', email: 'jworkman@fullsail.com'},
-        { name: 'Shelby Tufail', email: 'stufail@fullsail.com'},
-        { name: 'John Doe', email: 'jdoe@fullsail.com'},
-    ]})
+    res.render("views/home")
 })
  
 app.use("/products", productRouter)
+app.use("/variants", variantsRouter)
+app.use("/images", imagesRouter)
 
 app.listen(3000)
---
+++ routes/Variants.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const productCtrl = require('../controllers/Products');
+
+
+
+router.get('/', productCtrl.index);
+router.get('/new', productCtrl.form);
+router.get('/:id/edit', productCtrl.form);
+router.get('/:id', productCtrl.show);
+router.post('/', productCtrl.create);
+router.delete('/:id', productCtrl.remove);
+router.post('/:id', productCtrl.update);
+router.post('/:id/delete', productCtrl.remove);
+
+
+
+
+
+
+
+module.exports = router;
