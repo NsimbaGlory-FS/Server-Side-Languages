952977f99ae3d96e371d5110ed8aa9f4f1a804b2
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2023-11-11 10:26:29 -0500

    aaa

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const app = express()
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false}))
 const productRouter = require('./routes/Products')
+app.set('views', __dirname + '/templates')
+app.set('view engine', 'twig')
 
+app.get('/', (req, res) => {
+    res.render("home", { name: 'World!', 'users': [
+        { name: 'Justin Worman', email: 'jworkman@fullsail.com'},
+        { name: 'Shelby Tufail', email: 'stufail@fullsail.com'},
+        { name: 'John Doe', email: 'jdoe@fullsail.com'},
+    ]})
+})
+ 
 app.use("/products", productRouter)
 
 app.listen(3000)
