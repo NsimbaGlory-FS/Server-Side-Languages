75a702d4c703c8ea145e388c3e5687f207e9b7cf
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2023-11-18 09:54:17 -0500

    DD

--- migrations/20231117215156-createProduct.js
+++ migrations/20231117215156-createProduct.js
@@ -14,7 +14,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       price: {
-        type: Sequelize.INTEGER
+        type: Sequelize.FLOAT
       },
       createdAt: {
         allowNull: false,
