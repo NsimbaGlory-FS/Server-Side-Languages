461423454354515e3f919b31100257ccab97f0bb
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2023-11-18 08:58:26 -0500

    add

--- routes/Products.js
+++ routes/Products.js
@@ -6,11 +6,11 @@ const productCtrl = require('../controllers/Products');
 
 router.get('/', productCtrl.index);
 router.get('/new', productCtrl.form);
-router.get('/:id/edit', productCtrl.form);
 router.get('/:id', productCtrl.show);
+router.get('/:id/edit', productCtrl.form);
 router.post('/', productCtrl.create);
-router.delete('/:id', productCtrl.remove);
 router.post('/:id', productCtrl.update);
+router.delete('/:id', productCtrl.remove);
 router.get('/:id/delete', productCtrl.remove);
 
 
