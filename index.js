60397a81cb7c66eb42e9e45fee93d99623a3cdae
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2023-11-26 16:04:47 -0500

    ass

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-// GET /products/all HTTP/1.1, Page: 2, Sort: price, Order: descs
+// GET /products/all HTTP/1.1, Page: 2, Sort: price, Order: descss
 
 app.get('/products/:productName', (request, response) => {
     response.send('Product Page! Product name: Sort: price, Order: desc' + request.params.productName)
